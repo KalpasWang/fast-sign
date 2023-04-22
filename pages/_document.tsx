f0965d5a872e2703927c81ff68c1b29d5de47604
KalpasWang <wf6212@gmail.com>
2023-04-22 11:50:26 +0800

    add markup in index and add logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta
+          name='description'
+          content='好從容！您的電子簽署好夥伴，快速省時的電子簽署工具'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
